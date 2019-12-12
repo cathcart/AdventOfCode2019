60d09624ecd0ea0a70b65b4d1242a36f65fecd91
Thomas CATHCART <cathcart@Thomass-MacBook-Air.local>
2019-12-12 22:51:13 +0000

    day6 - part1 done

---
+++ day6/solution.py
@@ -0,0 +1,105 @@
+import collections
+import unittest
+from functools import reduce
+
+class Planet():
+	def __init__(self, name):
+		self.name = name
+		self.children = []
+		self.numberOfOrbits = 0
+
+	def orbit(self, planet):
+		if(planet not in self.children):
+			self.children.append(planet)
+
+	def setOrbits(self):
+		#print("hello from ", self.name)
+		for child in self.children:
+			child.numberOfOrbits = self.numberOfOrbits + 1
+			child.setOrbits()
+
+	def getOrbits(self):
+		total = self.numberOfOrbits
+		for child in self.children:
+			total += child.getOrbits()
+		return total
+
+	def __repr__(self):
+		return self.name
+
+class SolarMap():
+	def __init__(self, inputs):
+		self.planets = []
+		self.inputs = inputs
+
+	def run(self):
+		self.buildPlanetNames()
+		#print("planet names done")
+		self.buildPlanets()
+		#print("planets done")
+		self.fixOrbits()
+		#print("orbits done")
+
+	def buildPlanetNames(self):
+		
+		for x in self.inputs:
+			[a,b] = x.split(")")
+
+			if a not in self.planets:
+				self.planets.append(a)
+			if b not in self.planets:
+				self.planets.append(b)
+
+	def findPlanet(self, planetName):
+		return list(filter(lambda x: x.name == planetName, self.actual_planets))[0]
+
+	def buildPlanets(self):
+		self.actual_planets = list(map(lambda x: Planet(x), self.planets))
+
+		for x in self.inputs:
+			[a,b] = x.split(")")
+			A = self.findPlanet(a)
+			B = self.findPlanet(b)
+
+			A.children.append(B)
+			A.orbit(B)
+
+	def fixOrbits(self):
+
+		COM = self.findPlanet("COM")
+		COM.setOrbits()
+
+	def totalOrbits(self):
+
+		return sum(list(map(lambda x: x.numberOfOrbits, self.actual_planets)))
+
+
+
+class TestSolarMap(unittest.TestCase):
+	def run(self):
+
+		inputs = get_input("example.txt")
+		solMap = SolarMap(inputs)
+		solMap.run()
+		#print(solMap.actual_planets)
+		#print(solMap.findPlanet("K").children)
+
+		self.assertEqual(solMap.findPlanet("D").numberOfOrbits, 3)
+		self.assertEqual(solMap.findPlanet("L").numberOfOrbits, 7)
+		self.assertEqual(solMap.findPlanet("COM").numberOfOrbits, 0)
+		self.assertEqual(solMap.totalOrbits(), 42)
+
+
+def get_input(input_file):
+	return open(input_file).read().strip().split("\n")
+
+
+if __name__ == "__main__":
+	#TestSolarMap().run()
+
+	inputs = get_input("input.txt")
+	solMap = SolarMap(inputs)
+	solMap.run()
+
+	print(solMap.totalOrbits())
+
